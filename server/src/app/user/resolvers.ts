8c69c3bb6bbb8eccb26e1356c10019abf5fd9e02
Ravi <2140131@sliet.ac.in>
2024-10-28 01:41:52 +0530

    feat: resolver to create user with jwtToken

--- server/src/app/user/resolvers.ts
+++ server/src/app/user/resolvers.ts
@@ -1,6 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import axios from "axios";
+import { prisma } from "../../clients/db";
+import JWTServices from "../../services/jwt";
+
+interface GoogleTokenResult {
+    iss?: string;
+    nbf?: string;
+    aud?: string;
+    sub?: string;
+    email: string;
+    email_verified: string;
+    azp?: string;
+    name?: string;
+    picture?: string;
+    given_name: string;
+    family_name: string;
+    iat?: string;
+    exp?: string;
+    jti?: string;
+    alg?: string;
+    kid?: string;
+    typ?: string;
+}
+
 const queries = {
     verifyGoogleToken: async(parent: any, {token} : {token: string}) => {
-        return token
+        const googleToken = token;
+
+        const googleAuthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
+        googleAuthURL.searchParams.set('id_token', googleToken);
+
+        const { data } = await axios.get<GoogleTokenResult>(googleAuthURL.toString(), {
+            responseType: "json"
+        })
+        console.log(data);
+
+        const user = await prisma.user.findUnique({ where: { email : data.email }});
+
+        if(!user) {
+            await prisma.user.create({
+                data: {
+                    email: data.email,
+                    firstName: data.given_name,
+                    lastName: data.family_name,
+                    profileImageUrl: data.picture
+                }
+            })
+        }
+        const userInDb = await prisma.user.findUnique({ where: {email: data.email}});
+
+        if(!userInDb) throw new Error("User with email not found !");
+
+        const jwtToken = JWTServices.generateToken(userInDb);
+        console.log(jwtToken);
+        return jwtToken;
     }
 }
 
