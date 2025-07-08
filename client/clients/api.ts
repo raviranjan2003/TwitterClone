5be57fcd1af6e028f0ab6e38294f31e486d402ab
Ravi <2140131@sliet.ac.in>
2025-07-08 16:59:54 +0530

    fix: fixed typo

--- client/clients/api.ts
+++ client/clients/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request';
 
 const isClient = typeof window !== undefined
 
-export const graphqlCLient = new GraphQLClient("http://localhost:8000/graphql",
+export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql",
     {
         headers: () => ({
             Authorization: isClient ?
