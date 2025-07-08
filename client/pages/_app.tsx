ec09b2322fc3abbb026349e5480cb6d1886f42e0
Ravi <2140131@sliet.ac.in>
2025-07-08 17:02:21 +0530

    feat: wrapping project with TanstackQuery

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,10 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+//Creating client 
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <GoogleOAuthProvider clientId="286698739945-7btogsgjha7cus88rdbgctfrg4qlk7ko.apps.googleusercontent.com">
-    <Component {...pageProps} />
-    <Toaster />
-  </GoogleOAuthProvider>;
+  return <QueryClientProvider client={queryClient}>
+    <GoogleOAuthProvider clientId="286698739945-7btogsgjha7cus88rdbgctfrg4qlk7ko.apps.googleusercontent.com">
+      <Component {...pageProps} />
+      <Toaster />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </GoogleOAuthProvider>
+  </QueryClientProvider>;
 }
