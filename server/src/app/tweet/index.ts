01ff879c54a05ed8e29a8b60c95de0628cd9dc22
Ravi <2140131@sliet.ac.in>
2025-07-11 00:26:06 +0530

    query for getAllTweets

--- server/src/app/tweet/index.ts
+++ server/src/app/tweet/index.ts
@@ -1,5 +1,6 @@
 import { mutations} from "./mutations";
 import { types } from "./types";
 import { resolvers } from "./resolvers";
+import { queries } from "./queries";
 
-export const Tweet = { mutations, types, resolvers };
+export const Tweet = { mutations, types, resolvers, queries };
