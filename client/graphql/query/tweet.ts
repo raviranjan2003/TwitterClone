7ebcabd6eae955c6d2b302ceea6b350ae2014264
Ravi <2140131@sliet.ac.in>
2025-07-11 12:55:34 +0530

    query for getAllTweets

---
+++ client/graphql/query/tweet.ts
@@ -0,0 +1,16 @@
+import { graphql } from "@/gql";
+
+export const getAllTweetsQuery = graphql(`#graphql
+    query GetAllTweets {
+        getAllTweets {
+            id
+            content
+            imageUrl
+            author {
+                firstName
+                lastName
+                profileImageUrl
+            }
+        }
+    }
+`)
