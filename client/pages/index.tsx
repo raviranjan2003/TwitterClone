c969126611d1172f27799f644bd3e34f817ef54e
Ravi <2140131@sliet.ac.in>
2025-07-13 01:51:21 +0530

    feat: creating tweet with Image

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,6 +8,10 @@ import { BiImage } from "react-icons/bi";
 import { useCreateTweet, useGetAllTweets } from "@/hooks/tweet";
 import { Tweet } from "@/gql/graphql";
 import TwitterLayout from "@/components/FeedCard/Layout/TwitterLayout";
+import { graphqlClient } from "@/clients/api";
+import { getPresignedUrlQuery } from "@/graphql/query/tweet";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +31,7 @@ const geistMono = localFont({
 export default function Home() {
   //state for creating tweet
   const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   const { mutate } = useCreateTweet();
 
   const user = useCurrentUser();
@@ -35,21 +40,53 @@ export default function Home() {
   const { tweets } = useGetAllTweets();
   // console.log("tweet==>", tweets);
   
+  const handleInputChange = useCallback((input: HTMLInputElement) =>{
+    return async (event: Event) => {
+      event.preventDefault();
+      // console.log(input.files);
+      const file : File | null | undefined = input.files?.item(0);
+      if(!file) return;
 
+      const { getPresignedUrl } = await graphqlClient.request(getPresignedUrlQuery, {
+        imageName: file.name,
+        imageType: file.type
+      })
+
+      if(getPresignedUrl) {
+        toast.loading("Uploading...", { id: "2" });
+        await axios.put(getPresignedUrl, file, {
+          headers: {
+            'Content-Type': file.type
+          }
+        })
+
+        toast.success("File Uploaded !", { id: "2" });
+        const url = new URL(getPresignedUrl);
+        const filePath = `${url.origin}${url.pathname}`;
+        setImageUrl(filePath);
+      }
+    }
+  }, [])
  
 
   const handleImageUpload = useCallback(() => {
     const file = document.createElement("input");
     file.setAttribute("type", "file");
     file.setAttribute("accept", "image/*");
+
+    const handlerFn = handleInputChange(file);
+
+    file.addEventListener("change", handlerFn);
+
     file.click();
-  }, [])
+  }, [handleInputChange])
 
   const handleCreateTweet = useCallback(() => {
     mutate({
       content,
+      imageUrl
     })
-  },[content, mutate]);
+  },[content, mutate, imageUrl]);
 
   return (
    <div>
@@ -72,6 +109,9 @@ export default function Home() {
                   className="w-full h-20 bg-transparent border-b-2 border-slate-700 outline-none resize-none text-xl"
                   placeholder="What's happening?"
                 />
+                {
+                  imageUrl && <Image src={imageUrl} alt="Tweet-img" width={300} height={300} />
+                }
                 <div className="flex justify-between items-center">
                   <BiImage className="text-2xl" onClick={handleImageUpload} />
                   <button
