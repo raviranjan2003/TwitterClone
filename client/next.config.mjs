0f6736d877bf8b5ee9418574b4b4f4c4fd1d4fa9
Ravi <2140131@sliet.ac.in>
2025-07-13 01:49:55 +0530

    whitelisting s3-bucket hostname
    for image rendering

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -18,6 +18,11 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ravi-twitter-image-bucket.s3.ap-south-1.amazonaws.com',
+        pathname: '**',
+      },
     ],
   }
 };
