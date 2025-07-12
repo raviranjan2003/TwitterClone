b6eb03547ed97a13a1e6e4c2d05a75fb1d3b159b
Ravi <2140131@sliet.ac.in>
2025-07-12 14:06:55 +0530

    avoiding SSR to get access of localStorage
    localStorage is not available on server

--- client/clients/api.ts
+++ client/clients/api.ts
@@ -1,11 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
 
-const isClient = typeof window !== undefined
+const isClient = typeof window !== 'undefined'
 
 export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql",
     {
         headers: () => ({
-            Authorization: isClient ?
+            Authorization: isClient && typeof localStorage !== 'undefined' ?
                 `Bearer ${localStorage.getItem("twitter_token")}` : ""
         }),
     }
