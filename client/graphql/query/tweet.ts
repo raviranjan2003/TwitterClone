1a35a1fc6271a51cf78398ba47bf635a1fa6690e
Ravi <2140131@sliet.ac.in>
2025-07-12 14:09:08 +0530

    fetching authorId of each tweet

--- client/graphql/query/tweet.ts
+++ client/graphql/query/tweet.ts
@@ -7,6 +7,7 @@ export const getAllTweetsQuery = graphql(`#graphql
             content
             imageUrl
             author {
+                id
                 firstName
                 lastName
                 profileImageUrl
