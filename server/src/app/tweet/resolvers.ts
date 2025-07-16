cd4cbbde6e20675591467b69811c4510155522a4
Ravi <2140131@sliet.ac.in>
2025-07-16 14:03:21 +0530

    refactor: using TweetServices

--- server/src/app/tweet/resolvers.ts
+++ server/src/app/tweet/resolvers.ts
@@ -4,18 +4,15 @@ import { GraphqlContext } from "../../interface";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl} from "@aws-sdk/s3-request-presigner"
 import UserService from "../../services/user";
+import TweetService, { CreateTweetPayload } from "../../services/tweet";
 
-interface CreateTweetPayload {
-    content: string;
-    imageUrl: string
-}
 
 const s3Client = new S3Client({ 
     region: process.env.AWS_DEFAULT_REGION,
 });
 
 const queries = {
-    getAllTweets: () => prisma.tweet.findMany({ orderBy: { createdAt: "desc" }}),
+    getAllTweets: () => TweetService.getAllTweets(),
     getPresignedUrl: async (parent: any, { imageName, imageType }:{ imageName: string, imageType: string }, ctx: GraphqlContext)  => {
         if(!ctx.user || !ctx.user.id) throw new Error("Unauthenticated User!!");
 
@@ -39,13 +36,10 @@ const mutations = {
     createTweet: async (parent: any, { payload }:{ payload: CreateTweetPayload}, ctx: GraphqlContext) => {
         if(!ctx.user) throw new Error("You are not authenticated!");
 
-        const tweet = await prisma.tweet.create({
-            data: {
-                content: payload.content,
-                imageUrl: payload.imageUrl,
-                author: { connect: { id: ctx.user.id }}
-            }
-        })
+        const tweet = await TweetService.createTweet({
+            ...payload,
+            userId: ctx.user.id,
+        });
         return tweet;
     }
 }
