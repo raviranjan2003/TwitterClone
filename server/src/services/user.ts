68020bd4809a7f29cde3907e9662bb5bcc2ea16e
Ravi <2140131@sliet.ac.in>
2025-07-17 23:09:12 +0530

    UserService: follow unfollow service

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -57,6 +57,21 @@ class UserService {
     public static getUserById(id: string) {
         return prisma.user.findUnique({ where: { id }});
     }
+
+    public static followUser(from: string, to: string) {
+        return prisma.follows.create({
+            data: {
+                followers: { connect: { id: from }},
+                following: { connect: { id: to }},
+            }
+        })
+    }
+
+    public static unfollowUser(from: string, to: string) {
+        return prisma.follows.delete({
+            where: { followerId_followingId: { followerId: from, followingId: to }}
+        })
+    }
 }
 
 export default UserService;
